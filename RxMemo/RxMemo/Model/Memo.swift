8df0beb770dac77355c84de4510505c9e844b99d
Hoon94 <dleognsdl1@naver.com>
2024-11-20 23:15:33 +0900

    feat: Memo 모델 추가

---
+++ RxMemo/RxMemo/Model/Memo.swift
@@ -0,0 +1,30 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by Daehoon Lee on 11/20/24.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    
+    // MARK: - Properties
+    
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    // MARK: - Lifecycle
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
