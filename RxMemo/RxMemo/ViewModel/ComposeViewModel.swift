0bcb3f36bcf88a61f26ce52cc07638e6f65207ef
Hoon94 <dleognsdl1@naver.com>
2024-11-23 22:15:33 +0900

    feat: 쓰기 화면에서 사용하는 뷰 모델 구현

--- RxMemo/RxMemo/ViewModel/ComposeViewModel.swift
+++ RxMemo/RxMemo/ViewModel/ComposeViewModel.swift
@@ -5,8 +5,52 @@
 //  Created by Daehoon Lee on 11/21/24.
 //
 
+import Action
 import Foundation
+import RxCocoa
+import RxSwift
 
-class ComposeViewModel {
+class ComposeViewModel: CommonViewModel {
     
+    // MARK: - Properties
+    
+    private let content: String?
+    
+    var initialText: Driver<String?> {
+        return Observable.just(content).asDriver(onErrorJustReturn: nil)
+    }
+    
+    let saveAction: Action<String, Void>
+    let cancelAction: CocoaAction
+    
+    // MARK: - Lifecycle
+    
+    init(
+        title: String,
+        content: String? = nil,
+        sceneCoordinator: SceneCoordinatorType,
+        storage: MemoStorageType,
+        saveAction: Action<String, Void>? = nil,
+        cancelAction: CocoaAction? = nil
+    ) {
+        self.content = content
+        
+        self.saveAction = Action<String, Void> { input in
+            if let action = saveAction {
+                action.execute(input)
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        self.cancelAction = CocoaAction {
+            if let action = cancelAction {
+                action.execute(())
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
