b016cf02cd86c96cb10ec6b7c35c425f372b17a0
Hoon94 <dleognsdl1@naver.com>
2024-11-28 23:35:46 +0900

    feat: CoreDataStorage 생성

--- RxMemo/RxMemo/Model/Memo.swift
+++ RxMemo/RxMemo/Model/Memo.swift
@@ -5,7 +5,9 @@
 //  Created by Daehoon Lee on 11/20/24.
 //
 
+import CoreData
 import Foundation
+
 import RxDataSources
 
 struct Memo: Equatable, IdentifiableType {
@@ -27,5 +29,35 @@ struct Memo: Equatable, IdentifiableType {
     init(original: Memo, updatedContent: String) {
         self = original
         self.content = updatedContent
+    }
+}
+
+// MARK: - Persistable
+
+extension Memo: Persistable {
+    static var entityName: String {
+        return "Memo"
+    }
+    
+    static var primaryAttributeName: String {
+        return "identity"
+    }
+    
+    init(entity: NSManagedObject) {
+        content = entity.value(forKey: "content") as! String
+        insertDate = entity.value(forKey: "insertDate") as! Date
+        identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    func update(_ entity: NSManagedObject) {
+        entity.setValue(content, forKey: "content")
+        entity.setValue(insertDate, forKey: "insertDate")
+        entity.setValue("\(insertDate.timeIntervalSinceReferenceDate)", forKey: "identity")
+        
+        do {
+            try entity.managedObjectContext?.save()
+        } catch {
+            print(error)
+        }
     }
 }
